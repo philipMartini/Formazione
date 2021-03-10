04d3e5f2d8aeada20a906a89268c5a7a1bc9a6da
Filippo Martini <f.martini@campus.unimib.it>
2021-03-10 18:47:52 +0100

    Deadlock example

---
+++ 00_java_base/multiThreadingConcorrenza/src/deadlock/Main.java
@@ -0,0 +1,67 @@
+package deadlock;
+
+public class Main {
+	
+	public static Object lock1 = new Object();
+	public static Object lock2 = new Object();
+
+	public static void main(String[] args) {
+			new Thread1().start();
+			new Thread2().start();
+	}
+	
+	/*Questa versione provoca Deadlock immediato, T1 acquisce il primo lock e va in sleep, 
+	 * T2 acquisisce il secondo lock e va in sleep, in questo contesto T1 aspetta lock2 che possiede T2 e T2 aspetta
+	 * lock1 che T1 possiede => Deadlock*/
+	
+	private static class Thread1 extends Thread{
+		
+		@Override
+		public void run(){
+			synchronized(lock1){
+				System.out.println("Thread 1 has Lock1");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("Thread 1 waiting for lock2");
+				synchronized(lock2){
+					System.out.println("Thread 1 has Lock2");
+				}
+				
+				System.out.println("Thread 1 released Lock2");
+			}
+			System.out.println("Thread 1 releasing lock1...Exiting...");
+		}
+	}
+	
+	private static class Thread2 extends Thread{
+		
+		@Override
+		public void run(){
+			synchronized(lock2){
+				System.out.println("Thread 2 has Lock2");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("Thread 2 waiting for lock1");
+				synchronized(lock1){
+					System.out.println("Thread 2 has Lock1");
+				}
+				
+				System.out.println("Thread 2 released Lock1");
+			}
+			System.out.println("Thread 1 releasing lock2...Exiting...");
+		}
+		
+	}
+
+}
+
